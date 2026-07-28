8c94ecf6805b0d39dc19ad246057b93a4869721b
rankits <rankisinghthakur2001@gmail.com>
2026-07-28 19:15:13 +0530

    feat(backend): implement errorHandler, endpointNotFound, and validateResource middlewares

---
+++ backend/src/shared/middlewares/errorHandler.ts
@@ -0,0 +1,33 @@
+import { ZodError } from "zod";
+import { NextFunction, Request, Response } from "express";
+import { AppError } from "../utils/errors/AppError.js";
+import { errorResponse } from "../utils/response.js";
+
+export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
+  console.error("[ERROR] : ", err);
+
+  if (err instanceof ZodError) {
+    return res.status(400).json(errorResponse("Payload is incorrect or missing fields."));
+  }
+
+  if (err instanceof AppError && err?.code === "TOO_MANY_FAILED_ATTEMPTS") {
+    return res.status(403).json({
+      ...errorResponse(err.message),
+      code: "TOO_MANY_FAILED_ATTEMPTS",
+      data: err.data,
+    });
+  }
+
+  if (err instanceof AppError && err?.code === "PASSWORD_RESET_REQUIRED") {
+    return res.status(403).json({
+      ...errorResponse(err.message),
+      code: "PASSWORD_RESET_REQUIRED",
+    });
+  }
+
+  if (err instanceof AppError) {
+    return res.status(err.statusCode).json(errorResponse(err.message));
+  }
+
+  return res.status(500).json(errorResponse("Internal Server Error"));
+};
